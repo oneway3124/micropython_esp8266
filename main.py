2e23919204a2153fa26b58499f7f60c82a3165f1
oneway3124 <190025935@qq.com>
2018-10-07 23:35:02 +0800

    add more information upload to onenet

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ TOPIC = b"$dp"             #ONENET上传数据点需要传到此TOPIC
 username='176495'           #注册产品时，平台分配的产品ID，为数字字串
 password='wang'  #鉴权信息
 message="{\"hello\":3}"     #即{"hello":1},向名为hello的数据流传1 
+message1="{\"hello\":1}"     #即{"hello":1},向名为hello的数据流传1 
 
 msglen=len(message)
 tmp=[0,0,0]
@@ -19,6 +20,7 @@ tmp[0]='\x03'
 tmp[1]=msglen>>8
 tmp[2]=msglen&0XFF
 message="%c%c%c%s"%(tmp[0],tmp[1],tmp[2],message)    #将消息封装为ONENET要求的格式
+message1="%c%c%c%s"%(tmp[0],tmp[1],tmp[2],message1)    #将消息封装为ONENET要求的格式
 
 def main(server=SERVER):
     c = MQTTClient(CLIENT_ID, server,SERVER_PORT,username,password)
@@ -30,6 +32,8 @@ def main(server=SERVER):
             print(message)
             c.publish(TOPIC, message)
             time.sleep_ms(2000)
+            print(message1)
+            c.publish(TOPIC, message1)
 
     c.disconnect()
 
